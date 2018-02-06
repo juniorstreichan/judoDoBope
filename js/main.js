1536e974150c411a28a632d9006c1e4b8387f429
Junior Erdmann Streichan <juniorstreichan@gmail.com>
2018-02-06 10:40:16 -0300

    fotos, apoiaddores e sobre

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 $('nav a').click(function(e) {
-  e.preventDefault();
-  var id = $(this).attr('href'),
-  targetOffset = $(id).offset().top;
- $('html,body').animate({scrollTop: targetOffset - 50}, 600);
+	e.preventDefault();
+	var id = $(this).attr('href'),
+	targetOffset = $(id).offset().top;
+	$('html,body').animate({scrollTop: targetOffset - 50}, 600);
 });
+
+
+
+// apoio
+/* Type-1 */
+$('#myCarousel').carousel({
+	interval: 4000
+})
+
+$('.carousel-multi .item').each(function(){
+	var next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+	
+	for (var i=0;i<2;i++) {
+		next=next.next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+		
+		next.children(':first-child').clone().appendTo($(this));
+	}
+});
+
+
+
+////// 
